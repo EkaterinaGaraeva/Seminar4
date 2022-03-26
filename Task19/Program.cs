c8cea62195c64415c5152a6498dfb0d57b13c6f3
Ваше имя <garaevaea@mail.ru>
2022-03-26 10:24:38 +0300

    Add Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,25 @@
+﻿int fiveDigitNumber = Convert.ToInt32(Console.ReadLine());
+string returnMessage = $" {fiveDigitNumber}";
+if(fiveDigitNumber > 9999 && fiveDigitNumber < 100000)
+{
+    int[] digits = new int[5];
+    for (int i = 0; i < digits.Length; i++)
+    {
+        digits[i] = fiveDigitNumber % 10;
+        fiveDigitNumber /= 10;
+    }
+    if(digits[0] == digits[4] && digits[1] == digits[3])
+    {
+        returnMessage += "- это полиндром";
+    }
+    else
+    {
+        returnMessage += "- это не полиндром";
+    }
+}
+else 
+{
+    returnMessage += "- это не пятизначное число";
+}
+
+Console.WriteLine(returnMessage);
