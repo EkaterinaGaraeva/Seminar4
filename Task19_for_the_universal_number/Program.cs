62527b1dd8335fec1282d4201b65ee95a94fe125
Ваше имя <garaevaea@mail.ru>
2022-03-26 11:09:21 +0300

    Add Task19 for the universal number

---
+++ Task19_for_the_universal_number/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 19 для универсального числа
+
+int reverseNumber(int number)
+{
+    int reverseNumber = new int();
+    while (number > 0)
+    {
+        int lastDigit = number % 10;
+        reverseNumber = reverseNumber * 10 + lastDigit;
+        number /= 10;
+    }
+    return reverseNumber;
+}
+
+
+string IsNumberPolindrom(int number)
+{
+    string returnMessage = $" {number}";
+
+    int reversedNumber = reverseNumber(number);
+    if(reversedNumber == number)
+    {
+        returnMessage += "- это полиндром";
+    }
+    else
+    {
+        returnMessage += "- это не полиндром";
+    }
+    return returnMessage;
+}
+
+int number = Convert.ToInt32(Console.ReadLine());
+string returnMessage = IsNumberPolindrom(number);
+
+Console.WriteLine(returnMessage);
